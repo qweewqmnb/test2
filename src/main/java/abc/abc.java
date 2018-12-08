eebf15820c3c90b4fd4a2a2e0c217b31333fb8ca
oo <uioertert@163.com>
2018-12-08 20:48:57 +0800

    2047

--- src/main/java/abc/abc.java
+++ src/main/java/abc/abc.java
@@ -5,7 +5,7 @@ public class abc {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello   18470000000000000000000000000011111111112222222656666");
-		System.out.println("123456780");
+		System.out.println("1234567800000000000000000000000000000000000000000000000000000");
 		System.out.println("000000000000000000");
 		System.out.println("111111111111111111111111111111111111");
         System.out.println("9999999999999999999999999999999999999999999999");
